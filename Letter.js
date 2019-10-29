a79b3da32cf19625d576132b01d9a715cf5a1a6a
andrew stehno <andrew_stehno@yahoo.com>
2019-10-29 11:15:59 -0600

    created basic constructors in letter.js and word.js

--- Letter.js
+++ Letter.js
@@ -1,6 +1,21 @@
+let Letter = function(placeHolder) {
+    this.placeHolder = placeHolder,
+    this.value = false,
+    this.guess = function() {
+        if(this.value) {
+            return placeHolder;
+        }
+        return "_";
+    }
 
+    this.update = function(x) {
+        if (x === placeHolder) {
+            this.value = true;
+        }
+    }
+};
 
 
 module.exports = {
-// something to Letter.js
+    Letter
 };
--- Word.js
+++ Word.js
@@ -1,1 +1,13 @@
 let letter = require("./Letter");
+
+let Word = function(word) {
+    this.letter = word.split("").map(function(char){
+        return new letter(char);
+    })
+};
+
+Word.prototype.display = function () {
+    let displayString = "";
+
+    return 
+};
